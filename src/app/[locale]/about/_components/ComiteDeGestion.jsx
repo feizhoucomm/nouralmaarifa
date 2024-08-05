0e1dc4016987b8d2972a712ef6d6c330edb8028c
Ali-Oubraim <oubraima82@gmail.com>
2024-08-05 22:46:31 +0100

    update ComiteDeGestion Component

--- src/app/[locale]/about/_components/ComiteDeGestion.jsx
+++ src/app/[locale]/about/_components/ComiteDeGestion.jsx
@@ -1,5 +1,8 @@
 import { useTranslations } from "next-intl";
 import React from "react";
+import pres from "./../../../../../public/images/pres.png";
+import vice_pres from "./../../../../../public/images/vice-pres.png";
+import Image from 'next/image'
 
 const ComiteDeGestion = () => {
   const t = useTranslations("comite_de_gestion");
@@ -10,8 +13,8 @@ const ComiteDeGestion = () => {
       <div className="space-y-8 md:space-y-12">
         {/* Présidente */}
         <div className="flex flex-col md:flex-row items-center md:items-start gap-6">
-          <img
-            src="/chemin/vers/image-presidente.jpg"
+          <Image
+            src={pres}
             alt="Mme Mina Khasoumi"
             className="w-48 h-48 object-cover rounded-lg shadow-md"
           />
@@ -25,8 +28,8 @@ const ComiteDeGestion = () => {
 
         {/* Vice-Présidente */}
         <div className="flex flex-col md:flex-row items-center md:items-start gap-6">
-          <img
-            src="/chemin/vers/image-vice-presidente.jpg"
+          <Image
+            src={vice_pres}
             alt="Mme Khadija Afdal"
             className="w-48 h-48 object-cover rounded-lg shadow-md"
           />
@@ -34,7 +37,9 @@ const ComiteDeGestion = () => {
             <h2 className="text-xl font-semibold text-blue-700 mb-2">
               {t("vice_presidente.nom")}
             </h2>
-            <p className="text-gray-700 text-sm">{t("vice_presidente.formation")}</p>
+            <p className="text-gray-700 text-sm">
+              {t("vice_presidente.formation")}
+            </p>
           </div>
         </div>
       </div>
