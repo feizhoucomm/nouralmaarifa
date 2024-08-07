e583feea7baf6177f8465f4c2a85ca21fc94782f
Ali-Oubraim <oubraima82@gmail.com>
2024-08-07 13:35:17 +0100

    Some modification and fix some bugs

--- src/app/[locale]/page.js
+++ src/app/[locale]/page.js
@@ -1,10 +1,16 @@
 import HomePage from "./accueil/page";
 
+export const metadata = {
+  title: "Nour Al Maarifa",
+  description: "Association Marocaine de la Formation et le Conseil",
+  keywords:
+    "association, coaching, formation, randonnée, santé mentale, bien-être communautaire, développement de la communauté",
+};
 
 export default function Home() {
   return (
     <>
       <HomePage />
     </>
-   );
+  );
 }
