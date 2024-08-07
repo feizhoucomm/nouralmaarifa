068ee99a36c0334e558856703e285de7fff6804e
Ali-Oubraim <oubraima82@gmail.com>
2024-08-07 16:15:42 +0100

    Working on SEO using Metadata in home page

--- src/app/[locale]/page.js
+++ src/app/[locale]/page.js
@@ -1,23 +1,16 @@
 import Head from "next/head";
 import HomePage from "./accueil/page";
 
+export const metadata = {
+  title: "Nour Al Maarifa",
+  description: "Association Marocaine de la Formation et le Conseil",
+  keywords:
+    "association, coaching, formation, randonnée, santé mentale, bien-être communautaire, développement de la communauté",
+};
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>FeizhouCom - Blog</title>
-        <meta
-          name="description"
-          content="FeizhouCom l'agence qui donne vie à votre identité, reflétant fidèlement votre essence et vos valeurs."
-        />
-        <meta
-          name="keywords"
-          content="Sites web, Traduction, Communication, Relations publiques, Communication corporate, Image de marque"
-        />
-        <meta name="author" content="FeizhouCom" />
-        <link rel="icon" href="./FeizhouCom.png" type="image/png" />
-      </Head>
       <HomePage />
     </>
   );
